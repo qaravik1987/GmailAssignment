c2630d72c140c4cfd49ace61f523cb14b22db5cc
qaravik1987 <qaravik1987@gmail.com>
2016-11-17 20:13:21 +0530

    Updated code, bifurcated feature by created methods.

--- src/main/java/corilis/test/GmailAssignment.java
+++ src/main/java/corilis/test/GmailAssignment.java
@@ -10,22 +10,35 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GmailAssignment {
+	
+	public WebDriver driver;
+	
+	public void driverSetup()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//lib//chromedriver.exe");
+		driver = new ChromeDriver();
+	}
 
 	public static void main(String[] args){
-		
-		String content = "Name : Ravi Kumar Mahobia \n"+"Contact No : 8888817189 \n"+"http://";
-		System.setProperty("webdriver.chrome.driver", ".//lib//chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		GmailAssignment _gmailAssignment = new GmailAssignment();
+		_gmailAssignment.driverSetup();
+		_gmailAssignment.loginIntoGmail("qaravik1987@gmail.com", "test12345678");
+		_gmailAssignment.tearDown();
+	}
+	
+	public void loginIntoGmail(String userID, String pwd)
+	{
+		String content = "Name : Ravi Kumar Mahobia \n"+"Contact No : 8888817189 \n"+"https://github.com/qaravik1987/GmailAssignment.git";
 		WebDriverWait wait = new WebDriverWait(driver, 2);
 		String url = "https://www.gmail.com";
 		driver.get(url);
 		WebElement emailId = driver.findElement(By.id("Email"));
-		emailId.sendKeys("qaravik1987@gmail.com");
+		emailId.sendKeys(userID);
 		WebElement next = driver.findElement(By.id("next")); 
 		next.submit();
 		WebElement password = driver.findElement(By.id("Passwd")); 
 		wait.until(ExpectedConditions.elementToBeClickable(password));
-		password.sendKeys("test12345678");
+		password.sendKeys(pwd);
 		WebElement signIn = driver.findElement(By.id("signIn"));
 		signIn.submit();
 		WebElement compose = driver.findElement(By.cssSelector("div[class^='T-I'][role='button'][gh='cm']"));
@@ -43,9 +56,12 @@ public class GmailAssignment {
 		WebElement mailBody = driver.findElement(By.cssSelector("div[class^='Am']"));
 		mailBody.sendKeys(content);
 		WebElement send = driver.findElement(By.cssSelector("div[role='button'][data-tooltip^='Send']"));
-	//	send.click();
+	    send.click();
+	}
+	
+	public void tearDown()
+	{
 		driver.close();
-
 	}
 
 }
